9a5e76dd59cd6327cdff6cb04a95b82ad5df2114
gargisharma1998 <60867073+gargisharma1998@users.noreply.github.com>
2021-06-30 17:29:26 +0530

    Update myscript.groovy

--- vars/myscript.groovy
+++ vars/myscript.groovy
@@ -3,8 +3,7 @@ def info(message) {
 	//echo "\u001B[4;32m [INFO: ] \u001B[0m \u001B[1;33m ${message} \u001B[0m"  
 	
 	/* Both are working but the numbering matters, I don't know why, it should be in ascending order like 4;32m is valid but
-	32;4m is invalid*/
-	
+	32;4m is invalid*/	
 }
 def warning(message) {
     echo "WARNING: \033[1;31;43m${message}\033[0m"
