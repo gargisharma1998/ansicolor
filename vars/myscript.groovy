58bdc1874086c4d5b9de60722a5ac1ddd0d65aa5
gargisharma1998 <60867073+gargisharma1998@users.noreply.github.com>
2021-06-30 17:26:57 +0530

    Update myscript.groovy

--- vars/myscript.groovy
+++ vars/myscript.groovy
@@ -6,11 +6,9 @@ def info(message) {
 	32;4m is invalid*/
 	
 }
-
 def warning(message) {
     echo "WARNING: \033[1;31;43m${message}\033[0m"
 }
-
 def gitCommitId(message)
 {
       echo  "\033[4;32m[Git Commit ID] \033[0m \033[1;33m ${message} \033[0m\n"
